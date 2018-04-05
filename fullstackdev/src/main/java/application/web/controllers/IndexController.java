7b832dce9d811c098aacdd74788ba0050edef861
Viana <brunovianasp@gmail.com>
2018-04-05 11:58:24 -0300

    Index page created

---
+++ fullstackdev/src/main/java/application/web/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package application.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	
+}
