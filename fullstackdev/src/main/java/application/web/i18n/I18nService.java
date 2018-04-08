c269d204acd732a592930d0909949a0d8ab25107
ApokaliseGames <apokalisegames@gmail.com>
2018-04-08 01:21:27 -0300

    Added i18n capability to the web skeleton

---
+++ fullstackdev/src/main/java/application/web/i18n/I18nService.java
@@ -0,0 +1,24 @@
+package application.web.i18n;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class I18nService {
+
+	@Autowired
+	private MessageSource messageSource; 
+	
+	public String getMessage(String messageId) {
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		return getMessage(messageId, locale);
+	}
+
+	private String getMessage(String messageId, Locale locale) {
+		return messageSource.getMessage(messageId,  null, locale);
+	}
+}
--- fullstackdev/src/test/java/application/FullstackdevApplicationTests.java
+++ fullstackdev/src/test/java/application/FullstackdevApplicationTests.java
@@ -1,16 +1,42 @@
 package application;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import application.web.i18n.I18nService;
+
+//@AutoConfigureMockMvc
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@WebAppConfiguration
 public class FullstackdevApplicationTests {
 
+	@Autowired
+	I18nService i18nService;
+	
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() {
+		String expected = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessage(messageId);
+		assertEquals("The strings don't match", expected, actual);
+		
 	}
 
+	
 }
