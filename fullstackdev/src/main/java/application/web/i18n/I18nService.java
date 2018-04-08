2a05e2ad8f8afec0bfac5e5365da949ea7d4c06a
ApokaliseGames <apokalisegames@gmail.com>
2018-04-08 15:50:57 -0300

    Added logback with log4j

--- fullstackdev/src/main/java/application/web/i18n/I18nService.java
+++ fullstackdev/src/main/java/application/web/i18n/I18nService.java
@@ -5,16 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class I18nService {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(I18nService.class);
+
+	
 	@Autowired
 	private MessageSource messageSource; 
 	
 	public String getMessage(String messageId) {
-		Locale locale = LocaleContextHolder.getLocale();
-		
+		log.info("Returning i18n text for messageId {}", messageId);
+		Locale locale = LocaleContextHolder.getLocale();	
 		return getMessage(messageId, locale);
 	}
 
