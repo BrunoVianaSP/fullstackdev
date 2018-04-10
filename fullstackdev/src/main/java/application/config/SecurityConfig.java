00378133a4694120929ede3c93cc061b9cf2efb6
ApokaliseGames <apokalisegames@gmail.com>
2018-04-09 23:42:19 -0300

    Creating Spring Security

---
+++ fullstackdev/src/main/java/application/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package application.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import application.backend.services.UserSecurityService;
+//import application.web.controllers.ForgotMyPasswordController;
+//import application.web.controllers.SignupController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by tedonema on 26/03/2016.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Autowired
+//	private UserSecurityService userSecurityService;
+
+	@Autowired
+	private Environment env;
+
+	/** The encryption SALT. */
+	private static final String SALT = "fdalkjalk;3jlwf00sfaof";
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+	}
+
+//	/** Public URLs. */
+//	private static final String[] PUBLIC_MATCHERS = { "/webjars/**", "/css/**", "/js/**", "/images/**", "/",
+//			"/about/**", "/contact/**", "/error/**/*", "/console/**",
+//			ForgotMyPasswordController.FORGOT_PASSWORD_URL_MAPPING, ForgotMyPasswordController.CHANGE_PASSWORD_PATH,
+//			SignupController.SIGNUP_URL_MAPPING };
+	
+	/** Public URLs. */
+	private static final String[] PUBLIC_MATCHERS = { "/webjars/**", "/css/**", "/js/**", "/images/**", "/",
+			"/about/**", "/contact/**", "/error/**/*"};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+		if (activeProfiles.contains("dev")) {
+			http.csrf().disable();
+			http.headers().frameOptions().disable();
+		}
+
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").defaultSuccessUrl("/payload").failureUrl("/login?error").permitAll().and().logout()
+				.permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		//auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
+}
