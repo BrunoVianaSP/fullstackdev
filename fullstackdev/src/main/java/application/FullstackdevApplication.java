637de9592405fc9bd68e5bee784fcde08337f45e
ApokaliseGames <apokalisegames@gmail.com>
2018-05-01 20:23:07 -0300

    Externalised user configurations

--- fullstackdev/src/main/java/application/FullstackdevApplication.java
+++ fullstackdev/src/main/java/application/FullstackdevApplication.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,18 +28,28 @@ public class FullstackdevApplication implements CommandLineRunner {
 	@Autowired
 	private UserService userService;
 
+	@Value("${webmaster.username}")
+	private String webmasterUsername;
+
+	@Value("${webmaster.password}")
+	private String webmasterPassword;
+
+	@Value("${webmaster.email}")
+	private String webmasterEmail;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackdevApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		String userName = "proUser"; 
-		String email = "proUser@email.com";
+//		String userName = "proUser"; 
+//		String email = "proUser@email.com";
 		
-		User user = UsersUtils.createBasicUser(userName, email);
+		User user = UsersUtils.createBasicUser(webmasterUsername, webmasterPassword);
+		user.setPassword(webmasterPassword);
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user, new Role(RolesEnum.BASIC)));
+		userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
 		LOG.debug("Creating user with username {}", user.getUsername());
 		userService.createUser(user, PlansEnum.PRO, userRoles);
 		LOG.info("User {} created", user.getUsername());
--- fullstackdev/src/main/java/application/config/ApplicationConfig.java
+++ fullstackdev/src/main/java/application/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package application.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages="application.backend.persistence.repositories")
 @EntityScan(basePackages = "application.backend.persistence.domain.backend")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/.fullstackdev/application-commom.properties")
 public class ApplicationConfig {
 	
 }
